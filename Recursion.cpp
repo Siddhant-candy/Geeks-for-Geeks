a261fe372bd1e73920623f16128268320fc93fae
Siddhant-candy <63663101+Siddhant-candy@users.noreply.github.com>
2020-12-23 19:58:01 +0530

    Update Recursion.cpp

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,84 +1,174 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
+// Print n to 1 using recursion
 
-
-
-
-// Tail Recursive function for factorial :
-int fact(int n,int k)
+void printn(int n)
 {
-	if(n == 0 || n == 1)
+	if(n == 0)
+	return;
+	cout << n<<endl;
+	printn(n-1);
+}
+
+//Print 1 to n tail recursive
+
+void print1(int n,int k)
+{
+	if(k > n)
+	return;
+	
+	cout << k << endl;
+	print1(n,k+1);
+}
+
+//factorial non tail recursive
+
+long long factorial(int n)
+{
+	if(n == 0)
+	return 1;
+	return n*factorial(n-1);
+}
+
+//factorial tail recursive
+long long factorialn(int n ,int k)
+{
+	if(n == 0 || n==1)
 	return k;
 	
-	fact(n-1,k*n);
+	factorialn(n-1,k*n);
 }
 
-//Fibonacci
+// Sum of natural numbers
 
-int fib(int n)
+int getsum(int n)
 {
-	if(n == 0 || n == 1)
-	return n;
+	if(n==0)
+	return 0;
 	
-	return fib(n-1)+fib(n-2);
-}
-
-// sum of n natural numbers
-
-int sumofn(int n)
-{
-	if(n == 1)
-	return 1;
+	return n+getsum(n-1);
 	
-	return sumofn(n-1)+n;
 }
 
-//palindromeofstrings
 
-bool ispalindrome(string str,int i,int k)
+// Palindrome check
+
+bool isPalindrome(int n,string str,int len)
 {
-	if(i>=k)
+	if(n>=len)
 	return true;
 	
-	if(str[i] != str[k])
+	if(str[n] != str[len-1])
 	return false;
 	
-	ispalindrome(str,i+1,k-1);
+	isPalindrome(n+1,str,len-1);
+	
+	
+	
 	
 }
 
-// Sum of digits:
-int sumOfDigits(int n)
+
+// Sum of Digits
+
+int sumDigit(int n)
 {
 	if(n/10 == 0)
 	return n;
 	
-	return n%10 + sumOfDigits(n/10);
+	return sumDigit(n/10)+n%10;
+	
 }
 
-// Josephus
+//Rod Cutting
 
-int josephus (int n,int k)
+int rod(int n,int a,int b,int c)
 {
+	if(n==0)
+	{
+		return 0;
+	}
 	
+	if(n<a && n<b && n<c)
+	{
+		return 0;
+	}
 	
-		if(n == 1)
-		{
-			return 0;
-		}
-		
-		int i =  josephus(n-1,k)+k;
-	     
-		return i%n;	
+	int m1 = rod(n-a,a,b,c);
+	int m2 = rod(n-b,a,b,c);
+	int m3 = rod(n-c,a,b,c);
 	
-	
-	
+	return max(max(m1,m2),m3)+1;
 }
+
+
+
+//Subsets
+
+
+
+//Tower of Hanoi
+
+int toh(int n,int from ,int to,int aux)
+{
+	if(n==1)
+	{
+	 cout <<"move rod 1 from tower "<<from<<" to tower "<<to<<endl;
+	 return 1; 
+	}
+	
+	int a = toh(n-1,from,aux,to);
+	cout << "move rod " << n << " from tower " << from <<" to tower " << to<<endl;
+	int b = toh(n-1,aux,to,from);
+	
+	return a+b+1;
+}
+
+
+
+
+
+
+
+//Josephus  
+
+int jos(int n,int k)
+{
+  if(n == 1)
+  return 0;
+  
+  return (jos(n-1,k)+k)%n;
+}
+
+int josephus(int n,int k)
+{
+	return jos(n,k)+1;
+}
+
+
+
+
+
 
 main()
 {
-    //	cout << fact(5,1);
-    int n = 5,k=2;
-    cout << josephus(n,k)+1;
+	/*string s = "SIDDIS";
+    if(isPalindrome(0,s,s.length()) == 0)
+    cout << "True";
+    else
+    cout << "False";
+    
+    int n = 545;
+    cout << rod(5,3,2,1);
+    
+    cout << toh(3,1,3,2);*/
+    
+    cout << josephus(5,3);
 }
+
+
+	     
+		
